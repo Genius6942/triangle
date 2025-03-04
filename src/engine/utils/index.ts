cb387ade6c4a83a0b10141738dff62285c4ed4f8
halp <haelpf@gmail.com>
2025-03-04 18:06:28 -0500

    a faster deepCopy for performance

--- src/engine/utils/index.ts
+++ src/engine/utils/index.ts
@@ -5,4 +5,22 @@ export * from "./tetromino";
 export * from "./seed";
 export * from "./polyfills";
 
-export const deepCopy = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
+export const deepCopy = <T>(obj: T): T => {
+  if (!obj) return obj;
+  if (Array.isArray(obj)) {
+    var arr = [];
+    var length = obj.length;
+    for (var i = 0; i < length; i++) arr.push(deepCopy(obj[i]));
+    return arr as any;
+  } else if (typeof obj === "object") {
+    var keys = Object.keys(obj);
+    var length = keys.length;
+    var newObject: { [k: string]: any } = {};
+    for (var i = 0; i < length; i++) {
+      var key = keys[i];
+      newObject[key] = deepCopy((obj as any)[key]);
+    }
+    return newObject as any;
+  }
+  return obj;
+};
